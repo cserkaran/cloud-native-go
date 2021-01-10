ef55022bfb4ea3c4fd8a25fe30345c44f8cf80e1
Karan Sharma <7068938+cserkaran@users.noreply.github.com>
2021-01-10 16:27:24 +1100

    Retry pattern

--- retry/retry.go
+++ retry/retry.go
@@ -8,10 +8,12 @@ import (
 
 // Effector for retry logic. The function signature of the failing method,
 // which needs to be retried, must take the form of effector.
+// Effector is the function that interacts with the potentially failing service.
 type Effector func(context.Context) (string, error)
 
-// Retry functiion, which wraps the Effector function(the potentially failing method)
+// Retry function, which wraps the Effector function(the potentially failing method)
 // and adds the retry logic.
+// Retry function accepts Effector and returns a closure with the same function signature as Effector.
 func Retry(effector Effector, retries int, delay time.Duration) Effector {
 	return func(ctx context.Context) (string, error) {
 		for r := 0; ; r++ {
