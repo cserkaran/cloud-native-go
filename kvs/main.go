9954982d2f68385829a1264762b766fdf80af7c1
Karan Sharma <7068938+cserkaran@users.noreply.github.com>
2021-01-10 22:25:07 +1100

    Implemented basic Rest API.

---
+++ kvs/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/cloud-native-go/kvs/api"
+	"github.com/gorilla/mux"
+)
+
+// keyValuePutHandler expects to be called with a PUT request for
+// the "/v1/key/{key}"
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = api.Put(key, string(value))
+
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+// keyValueGetHandler expects to be called with a PUT request for
+// the "/v1/key/{key}"
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := api.Get(key)
+
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    key := vars["key"]
+
+    err := api.Delete(key)
+
+    if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+    w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Register keyValuePutHandler as the handler function for PUT
+	// requests matching "/v1/{key}"
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+
+	// Register keyValueGetHandler as the handler function for GET
+	// requests matching "/v1/{key}"
+    r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+    
+    // Register keyValueGetHandler as the handler function for DELETE
+	// requests matching "/v1/{key}"
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
