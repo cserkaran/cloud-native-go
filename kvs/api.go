9317112819bc596961b8e6c347eabd2415550297
Karan Sharma <7068938+cserkaran@users.noreply.github.com>
2021-01-10 21:31:12 +1100

    Generation 0 of Cloud Native Key Value store.

---
+++ kvs/api.go
@@ -0,0 +1,34 @@
+// Key value store api.
+
+package api
+
+import "errors"
+
+var store = make(map[string]string)
+
+// Put the value into the key.
+func Put(key,value string) error{
+    store[key] = value
+    return nil
+}
+
+//ErrorNoSuchKey error value indicating key does not exist.
+var ErrorNoSuchKey = errors.New("no such key")
+
+// Get the value for a key. Returns empty string and error in case 
+// key does not exist.
+func Get(key string)(string, error){
+    value, ok := store[key]
+
+    if !ok{
+        return "", ErrorNoSuchKey
+    }
+
+    return value, nil
+}
+
+// Delete the key.
+func Delete(key string) error{
+    delete(store,key)
+    return nil
+}
