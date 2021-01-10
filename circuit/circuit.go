b69de1e98ed90ec551b3350b2770b97936a12485
Karan Sharma <7068938+cserkaran@users.noreply.github.com>
2021-01-10 16:12:00 +1100

    Circuit breaker pattern

---
+++ circuit/circuit.go
@@ -0,0 +1,25 @@
+package circuit
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"time"
+)
+
+// The Circuit function, which interacts with the potentially failing service.
+type Circuit func(context.Context) (string, error)
+
+// New constructor to create the circuit function.
+// Returns an anonymouse function which fails intermittently.
+// Failure condition is randomized. 
+func New() (func(context.Context)(string,error)){
+    return func(ctx context.Context) (string, error) {
+		time.Sleep(3 * time.Second)
+		random := rand.Intn(100)
+		if random%3 == 0 {
+			return "success", nil
+		}
+		return "", errors.New("error calling circuit logic")
+	}
+}
